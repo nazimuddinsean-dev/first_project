5ffd6de1d6688c7956c5d2b387e3138e862368f4
“nu-sean” <nazim.portonics@gmail.com>
2026-01-04 10:26:42 +0600

    Initial Commit

---
+++ ProjectV/ProjectVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectVApp.swift
+//  ProjectV
+//
+//  Created by Nazim on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
